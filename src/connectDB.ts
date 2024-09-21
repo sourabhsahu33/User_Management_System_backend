35f7b7d3acb5fcf4514da3c546760ea2faa89ae3
learn github <sourabh.sahu10033@gmail.com>
2024-09-21 19:53:17 +0530

    first ccommit

--- src/connectDB.ts
+++ src/connectDB.ts
@@ -2,7 +2,8 @@ import mongoose, { ConnectOptions } from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/user_management', {
+    await mongoose.connect('MONGODB_URI=mongodb+srv://user123:sourabh@cluster0.t6kxsum.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     } as ConnectOptions);  // Ensure this is cast to ConnectOptions
