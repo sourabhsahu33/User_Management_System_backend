ba74bb879e9c3b21d92815384eb6d52cdad9740b
Sourabh Sahu <104298270+sourabhsahu33@users.noreply.github.com>
2024-09-21 20:03:21 +0530

    Update connectDB.ts

--- src/connectDB.ts
+++ src/connectDB.ts
@@ -2,14 +2,14 @@ import mongoose, { ConnectOptions } from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/user_management', {
+    await mongoose.connect('mongodb+srv://user123:sourabh@cluster0.t6kxsum.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    } as ConnectOptions);  // Ensure this is cast to ConnectOptions
+    } as ConnectOptions);  // Cast to ConnectOptions
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection failed:', error);
-    process.exit(1);
+    process.exit(1);  // Exit process with failure
   }
 };
 
